b694bab636c18391075737ca29dedca4beb124b5
Aerisfan01 <19flaszatommy@gmail.com>
2023-02-28 19:48:21 +1100

    yeppers

---
+++ multicalc.py
@@ -0,0 +1,12 @@
+print("Addition = +"  )                     #Menu for Operations
+print("Subtraction = -"  )
+print("Mutiplication = *"  )
+print("Division = /"  )
+
+Number1 = int(input("what is the first number?"))   #input of first number
+MathFunc = (input("What is the operation"))           #Function for calculation
+Number2 = int(input("what is the Second number?"))  # Secondary Number    
+
+Answer=Number1,MathFunc,Number2
+
+print("the Answer is ", Answer) #Print the answer and solve   
