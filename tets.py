5b4a5a9b48c37a498e440c7f8a83f3c00621a8b6
Aerisfan01 <126431658+Aerisfan01@users.noreply.github.com>
2023-02-28 15:26:53 +1100

    Add files via upload

---
+++ tets.py
@@ -0,0 +1,50 @@
+import random
+
+# Basic Moveset and RNG battle encounter   
+class Player:
+    def __init__(self, name, hp, attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+
+player1= Player("Cloud", 200, 20,5)
+
+
+move = input('Cross Slash, Braver, Ascention')
+input =  ('Cross Slash')
+print ('Cross Slash')
+hit = random.randint(1,100)
+
+
+
+
+class Enemy:
+    def __init__(self, name, hp , attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+
+
+
+
+Enemy1 = Enemy("Sephrioth", 200,20,5)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+    
+
+
